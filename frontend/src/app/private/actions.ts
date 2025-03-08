8c89b3a64387468e7120f1035930d7a9df3d8251
IshmamF <ishmamf2003@gmail.com>
2025-03-08 13:36:52 -0500

    function to get all data

---
+++ frontend/src/app/private/actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+
+export async function getSurroundingData () {
+    const supabase = await createClient();
+
+    const response = await supabase
+                    .from('posts')
+                    .select('title, description, image_url, longitude, latitude, severity, id');
+
+    if (response.error) {
+        console.error(response.error.message)
+        return []
+    };
+    
+    return Promise.resolve(response.data)
+}
